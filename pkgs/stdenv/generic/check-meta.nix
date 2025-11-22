f382328b3f87b5ca7a803a3b3b48df2f919b45af
Coutinho de Souza <dev@onemoresuza.com>
2025-11-21 20:26:03 -0400

    nixpkgs: create permittedUnfreePackages
    The created option lives side by side with allowUnfreePredicate, which
    has precedence over it.
    
    Should-cherry-pick: true

--- pkgs/stdenv/generic/check-meta.nix
+++ pkgs/stdenv/generic/check-meta.nix
@@ -136,8 +136,12 @@ let
   # {
   #   allowUnfree = false;
   #   allowUnfreePredicate = (x: pkgs.lib.hasPrefix "vscode" x.name);
+  #   permittedUnfreePackages = [ pkgs.steam ];
   # }
-  allowUnfreePredicate = config.allowUnfreePredicate or (x: false);
+  allowUnfreePredicate =
+    config.allowUnfreePredicate or (
+      x: builtins.elem (lib.getName x) (builtins.map lib.getName (config.permittedUnfreePackages or [ ]))
+    );
 
   # Check whether unfree packages are allowed and if not, whether the
   # package has an unfree license and is not explicitly allowed by the
